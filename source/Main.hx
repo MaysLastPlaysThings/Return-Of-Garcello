7dfc0cb86cda5201fe13251d2a19e4463d50aca5
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2024-04-17 16:25:25 +0300

    update source/Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -83,7 +83,7 @@ class Main extends Sprite
 			gameHeight = Math.ceil(stageHeight / zoom);
 		}
 
-		#if cpp
+		#if (cpp && !mobile)
 		initialState = Caching;
 		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
 		#else
